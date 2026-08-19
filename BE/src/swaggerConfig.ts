c7cea13aafe373b2cb460d9890dc55e82179d99e
Shrreya <shrreyaram@gmail.com>
2026-08-19 15:52:58 +0530

    Updated MessiQ

--- BE/src/swaggerConfig.ts
+++ BE/src/swaggerConfig.ts
@@ -4,7 +4,7 @@ const options: swaggerJsdoc.Options = {
     definition: {
         openapi: "3.0.0",
         info: {
-            title: "DM Agent SaaS - Node.js Backend API",
+            title: "Ykno Messiq Backend API",
             version: "1.0.0",
             description: "API documentation for the master backend service",
         },
